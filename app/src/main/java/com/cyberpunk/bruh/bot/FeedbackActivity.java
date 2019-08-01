564f1a145c1c6e6be10dd01d6c7adca37da5734b
NaitikVora <53094095+NaitikVora@users.noreply.github.com>
2019-08-01 20:23:02 +0530

    Initial commit

---
+++ app/src/main/java/com/cyberpunk/bruh/bot/FeedbackActivity.java
@@ -0,0 +1,25 @@
+package com.cyberpunk.bruh.bot;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+import android.annotation.TargetApi;
+
+public class FeedbackActivity extends Activity {
+    private WebView mWebview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    }
+
